a2625c235873c3dc0726a60d1db42a44e0a5553b
James Buck <130887820+BuckJ12@users.noreply.github.com>
2025-03-12 04:48:17 -0400

    Fix Gunicorn app import issue

--- Flask/app.py
+++ Flask/app.py
@@ -7,12 +7,10 @@ from routes.professors import professors_blueprint
 from routes.rooms import rooms_blueprint
 from routes.time_slots import time_slots_blueprint
 from routes.scheduler import scheduler_blueprint
-#from routes.auth import auth_blueprint
 from flask_cors import CORS
 import pymysql
+
 pymysql.install_as_MySQLdb()
-
-
 
 def create_app():
     app = Flask(__name__)
@@ -20,19 +18,21 @@ def create_app():
     db.init_app(app)
 
     CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
-    # Create the database tables if they don't exist.
+
     with app.app_context():
         db.create_all()
 
-    # Register our blueprint
+    # Register blueprints
     app.register_blueprint(courses_blueprint)
     app.register_blueprint(professors_blueprint)
     app.register_blueprint(rooms_blueprint)
     app.register_blueprint(time_slots_blueprint)
     app.register_blueprint(scheduler_blueprint)
-    # app.register_blueprint(auth_blueprint)
+
     return app
 
+# Create the app instance at the module level
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True)
